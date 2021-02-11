2b3b66b95c650eeed387308045b31c9057a8264e
HeartBrokenGod <neelwanky@gmail.com>
2021-02-12 01:49:25 +0530

    variables.c added

---
+++ variables.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    //  variable declaration
+    //  syntax
+    //  data_type variable_name ;
+    int num1;   // create a container of type int in ram
+    float num2; // create a container of type float in ram
+    long  num3; // create a container of type long in ram
+    char c;   // create a container of type char in ram
+    double num4;   // create a container of type double in ram
+
+
+    //  variable assignment
+    //	means to set value to container
+    //  syntax
+    //  variable_name = (value)
+    num1 = 90;
+    num2 = 86.20f;  // 'f' at the end of value is required
+    num3 = 46546464 ;
+    c = 'S';
+    num4 = 2335.1544 ; // no 'd' at the end of value
+
+
+}
