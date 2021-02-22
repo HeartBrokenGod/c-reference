46c1823adc2bdefc9bb4f59d2c05e7edf4d578a8
HeartBrokenGod <neelwanky@gmail.com>
2021-02-22 16:26:45 +0530

    practice progem number_pattern_2.c added to practice_program dir

---
+++ pracitce_programs/number_pattern_2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main()
+{
+
+    int number;
+
+
+
+    printf("Enter number : ");
+    scanf("%d",&number);
+
+    printf("\n");
+
+    for( int i = 0; i < number; i++  )
+    {
+        for( int j = 0; j < number; j++  )
+        {
+            if( i == 0 || i == (number - 1) )
+            {
+                printf("%d",number);
+            }
+            else
+            {
+                if( j == 0 || j == (number - 1) )
+                {
+                    printf("%d",number);
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+
+
+
+    getch();
+}
+
+
+
