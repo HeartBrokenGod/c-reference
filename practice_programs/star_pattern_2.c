a232a9b78e58d1b8dc79ca417fb3d2e5b4e051b3
HeartBrokenGod <neelwanky@gmail.com>
2021-03-02 00:24:24 +0530

    star_pattern_2.c is added to programs dir

---
+++ practice_programs/star_pattern_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    printf("\n\n================= Star Pattern 2 Program ==================\n\n");
+    printf("Enter a number : ");
+    int num;
+    scanf("%d",&num);
+    printf("\n");
+    //  calling function starPattern1
+    starPattern1(num);
+    printf("\n\n================= Star Pattern 2 Program ==================\n\n");
+    getch();
+}
+
+
+//  created separated function for printing pattern
+void starPattern1(int num){
+
+    //  this loop switches the line
+    for(int linePosition = 0; linePosition < num; linePosition++){
+
+        //  this loop switches the cursor Position
+        for(int cursorPosition = num; cursorPosition > linePosition; cursorPosition-- ){
+
+            //  printing logic
+            //  print num star on line 1
+            //  print ( num - 1 ) star on line 2
+            //  and so on
+            printf("* ");
+
+        }
+
+        //  this print switches the line actually
+        printf("\n");
+    }
+}
